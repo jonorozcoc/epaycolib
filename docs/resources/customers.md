4efd191128423686e00c87fd4fd28e7ee9c9c309
Jonatan Orozco <jonorozcoc@gmail.com>
2021-04-08 13:50:38 -0500

    fix(docs) corregido error documentacion

---
+++ docs/resources/customers.md
@@ -0,0 +1,4 @@
+---
+id: customers
+title: Clientes
+---
--- sidebars.js
+++ sidebars.js
@@ -2,6 +2,6 @@ module.exports = {
   someSidebar: {
     epaycolib: ["intro", "keys"],
     Funciones: ["features/payments", "features/suscriptions", "features/tokenization"],
-    Recursos: ["resources/document_types"]
+    Recursos: ["resources/customers", "resources/document_types"]
   },
 };
